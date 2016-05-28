9e43a8679daa89166690ab069c99f06e0a9d7b9f
Pisces000221 <1786762946@qq.com>
2016-05-28 18:14:27 +0800

    Improve tree listing

--- kuaipan/main.lua
+++ kuaipan/main.lua
@@ -4,32 +4,49 @@ inspect = inspect or require('./libs/inspect')
 
 http.default_referer = 'http://web.kuaipan.cn/n/drive/files'
 
-function dir_info_new(path, id, isdir, size, ctime, mtime)
-    local ret = {
+local files = {}
+local last_save = 0
+local save_interval = 10
+function serialize(record)
+    local s = record.id .. ','
+    if record.isdir then s = s .. 'DIR' else s = s .. record.size end
+    s = s .. ',' .. record.ctime .. ',' .. record.mtime .. ',' .. record.path .. '\n'
+    return s
+end
+function file_info_append(path, id, isdir, size, ctime, mtime)
+    local record = {
         path = path, id = id, isdir = isdir, size = size, ctime = ctime, mtime = mtime,
     }
-    if is_dir then ret.children = {} end
-    return ret
+    files[#files + 1] = record
+    if #files > last_save + save_interval then
+        f = io.open('dirlist.txt', 'a')
+        for i = last_save + 1, #files do
+            --f:write(i, ' ', inspect(files[i], { newline = '', indent = '' }), '\n')
+            f:write(i, ',', serialize(files[i]))
+        end
+        last_save = #files
+        f:close()
+    end
 end
 
-function ls(id, path)
+function ls(id)
     if id ~= nil then id = tostring(id) else id = '' end
-    print('Retrieving directory list: (' .. id .. ') ' .. path)
     local res = JSON:decode(http.post('http://web.kuaipan.cn/n/drive/getFiles', { id = id, sortby = '', cc = 'false' }))
     if res[1] ~= nil and res[1].docs ~= nil then
-        local ret = {}
-        res = res[1].docs
-        for i = 1, #res do
-            ret[#ret + 1] = dir_info_new(
-                res[i].name, res[i].id,
-                res[i].type == 'folder',
-                res[i].bytes,
-                res[i].ctime, res[i].mtime
+        local list = res[1].docs
+        if res[1].path == '/' then res[1].path = '' end
+        for i = 1, #list do
+            file_info_append(
+                res[1].path .. '/' .. list[i].name, list[i].id,
+                list[i].type == 'folder',
+                list[i].bytes,
+                list[i].ctime, list[i].mtime
             )
         end
-        for i = 1, #ret do
-            if ret[i].isdir then
-                ret[i].children = ls(ret[i].id, path .. ret[i].path .. '/')
+        for i = 1, #list do
+            if list[i].type == 'folder' then
+                print('Retrieving directory list: (' .. id .. ') ' .. res[1].path .. '/' .. list[i].name)
+                ls(list[i].id)
             end
         end
         return ret
