1e8bb8330dfa67a358c81d373c5d013122ff1f3e
Pisces000221 <1786762946@qq.com>
2016-05-29 19:00:20 +0800

    Make directories before downloading

--- kuaipan/download.lua
+++ kuaipan/download.lua
@@ -6,11 +6,16 @@ http.default_referer = 'http://web.kuaipan.cn/n/drive/files'
 http.xsrf_token = 'jsPTvIiZ-OKwTu6htxE4dMjgxkqZP-w8cOhM'
 
 local list_file = arg[1] or 'dirlist.csv'
-local dir_ct, file_ct, tot_size = 0, 0, 0
-local files = {}
+local base_dir = arg[2] or ''
+
+local dirs, files = {}, {}
+local tot_size = 0
 function deserialize(record)
     local ret = {}
-    record:gsub('([^,]+)', function (s) ret[#ret + 1] = s end)
+    record:gsub('([^,]+)', function (s)
+        if #ret < 6 then ret[#ret + 1] = s
+        else ret[6] = ret[6] .. ',' .. s end
+    end)
     return table.unpack(ret)
 end
 local byte_unit = { ['B'] = 1, ['KB'] = 1<<10, ['MB'] = 1<<20, ['GB'] = 1<<30, ['TB'] = 1<<40 }
@@ -29,13 +34,16 @@ function format_size(s)
     return string.format('%.2f %s', num, unit)
 end
 function file_info_append(num, id, size, ctime, mtime, path)
-    if size == 'DIR' then dir_ct = dir_ct + 1
+    if size == 'DIR' then
+        dirs[#dirs + 1] = {
+            path = path, id = id, ctime = ctime, mtime = mtime,
+        }
     else
         size = parse_size(size)
         files[#files + 1] = {
             path = path, id = id, size = size, ctime = ctime, mtime = mtime,
         }
-        file_ct = file_ct + 1; tot_size = tot_size + size
+        tot_size = tot_size + size
     end
 end
 function read_file()
@@ -49,20 +57,34 @@ function read_file()
 end
 
 function download_one(id, path)
-    http.download('http://web.kuaipan.cn/n/s3/getDownload?id=' .. id, path)
+    http.download('http://web.kuaipan.cn/n/s3/getDownload?id=' .. id, base_dir .. path)
+end
+function cmd_escape(s)
+    return s:gsub('%$', '\\$')
+end
+function prepare_downloads()
+    local i
+    -- NOTE: Can also be replaced with lfs
+    -- But here we just use os.execute for the sake of convenience
+    local s = 'mkdir ' .. base_dir
+    for i = 1, #dirs do
+        s = s .. ' "' .. base_dir .. cmd_escape(dirs[i].path) .. '"'
+        if i % 100 == 0 then os.execute(s); s = 'mkdir' end
+    end
+    if s ~= 'mkdir' then os.execute(s) end
 end
 function start_downloads()
     --local i
     --for i = 1, #files do
     --    download_one(files[i].id, files[i].path)
     --end
-    download_one(files[2].id, files[2].path:sub(2))
+    download_one(files[2].id, files[2].path)
 end
 
 print('The file to read from is [' .. list_file .. '].')
 local start_time = os.clock()
 read_file()
-print(dir_ct .. ' directory(-ies) and ' .. file_ct .. ' file(s) in total. Please confirm.')
+print(#dirs .. ' directory(-ies) and ' .. #files .. ' file(s) in total. Please confirm.')
 print('Total size is ~' .. format_size(tot_size) .. ' / ' .. math.floor(tot_size) .. ' Bytes'
     .. ' (estimated, actual size may differ a lot')
 print('Elapsed time: ' .. tostring(os.clock() - start_time) .. ' second(s)')
@@ -70,5 +92,6 @@ print('Elapsed time: ' .. tostring(os.clock() - start_time) .. ' second(s)')
 start_time = os.time()
 print('--------')
 print('Starting downloads.')
+prepare_downloads()
 start_downloads()
 print('Elapsed time: ' .. tostring(os.time() - start_time) .. ' second(s)')
